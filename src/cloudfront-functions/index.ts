20765b039c1586ed24bd00333e93873f1af72300
auvred <aauvred@gmail.com>
2024-10-02 16:49:52 +0300

    feat(cloudfront-functions): add ReplaceInURI function

--- src/cloudfront-functions/index.ts
+++ src/cloudfront-functions/index.ts
@@ -37,3 +37,59 @@ export class RewriteForSpa extends aws.cloudfront.Function {
     )
   }
 }
+
+export interface ReplaceInURIArgs {
+  pattern: RegExp
+  replacement: string
+}
+
+/**
+ * Replace some part of request uri
+ *
+ * @example
+ *
+ * ```ts
+ * // Replaces `/api/some-route` to `/some-route`
+ * new ReplaceInURI('name', {
+ *   pattern: /^\/api/,
+ *   replacement: '',
+ * })
+ * ```
+ *
+ * @example
+ *
+ * ```ts
+ * // Replaces `/api/api/some-route` to `/api/some-route`
+ * new ReplaceInURI('name', {
+ *   pattern: /\/api/g,
+ *   replacement: '',
+ * })
+ * ```
+ */
+export class ReplaceInURI extends aws.cloudfront.Function {
+  constructor(
+    name: string,
+    args: ReplaceInURIArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    const escapedPattern = (
+      typeof args.pattern === 'string' ? new RegExp(args.pattern) : args.pattern
+    ).toString()
+
+    super(
+      name,
+      {
+        code: `function handler(event) {
+  const request = event.request;
+  request.uri = request.uri.replace(${escapedPattern}, ${JSON.stringify(
+    args.replacement,
+  )});
+  return request;
+}`,
+        runtime: 'cloudfront-js-2',
+        publish: true,
+      },
+      opts,
+    )
+  }
+}
